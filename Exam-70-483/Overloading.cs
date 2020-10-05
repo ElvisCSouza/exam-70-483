236bbf79f493f86b3bdf31798a4455ccebf62b4d
Elvis Souza <elviscristiansouza@gmail.com>
2020-10-05 19:09:50 -0300

    Override e Overloading

---
+++ Exam-70-483/Overloading.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_70_483
+{
+    /// <summary>
+    /// Ocorre quando um método possui os mesmos tipos de retorno e mesmos parâmetros porém parâmetros diferentes.
+    /// </summary>
+    public class OverloadingExample
+    {
+        public void FazAlgo()
+        {
+
+        }
+        public void FazAlgo(int a)
+        {
+
+        }
+    }
+}
---
+++ Exam-70-483/Override.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_70_483
+{
+    /// <summary>
+    /// Ocorre quando um método possui os mesmos tipos de retorno e mesmos parâmetros porém parâmetros diferentes.
+    /// </summary>
+    public class OverrideExample
+    {
+        public virtual void FazAlgo(int a)
+        {
+
+        }
+    }
+
+    public class OverrideExampleChild : OverrideExample
+    {
+        public override void FazAlgo(int a)
+        {
+
+        }
+    }
+}
